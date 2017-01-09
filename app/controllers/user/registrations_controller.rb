521cbebc0fe1dce08fd18653f9108a503ae43ab9
Juan Lebrijo <juan@lebrijo.com>
2017-01-09 15:02:57 +0100

    fix logout

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -22,13 +22,5 @@ class User
       end
     end
 
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name,
-                                                         :last_name])
-      devise_parameter_sanitizer
-          .permit(:account_update, keys: [:first_name, :last_name, :company,
-                                          :country, :time_zone])
-    end
-
   end
 end
