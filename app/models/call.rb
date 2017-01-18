0d65a15695e4974b627303a5aff7eaf1a6fac64d
Juan Lebrijo <juan@lebrijo.com>
2017-01-18 12:19:21 +0100

    UR_17 Past/upcoming calls tab simple

--- app/models/call.rb
+++ app/models/call.rb
@@ -17,6 +17,8 @@ class Call < ApplicationRecord
   columns_filtered :title, :participant_code
 
   scope :most_recent, -> { where('date_time < ?', Time.now).order(:date_time) }
+  scope :upcoming, -> { where('date_time > ?', Time.now).order(:date_time) }
+  scope :past, -> { where('date_time < ?', Time.now).order(:date_time) }
 
   def past?
     Time.zone = time_zone
