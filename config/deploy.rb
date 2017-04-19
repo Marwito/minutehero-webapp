311c27c4a76677675ca1f49975bc0da045144be4
Juan Lebrijo <juan@lebrijo.com>
2017-04-19 13:04:45 +0200

    Fixing Facebook

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,7 +17,7 @@ set :bundle_binstubs, nil
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 # set :keep_releases, 5
 
-set :default_env, { path: "/home/ubuntu/.nvm/versions/node/v7.3.0/bin:$PATH" }
+set :default_env, { path: "/home/ubuntu/.nvm/versions/node/v7.3.0/bin:/home/ubuntu/.nvm/versions/node/v7.9.0/bin:$PATH" }
 
 namespace :deploy do
   # desc 'Restart application'
