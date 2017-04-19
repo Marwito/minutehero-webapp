a8167d4436b178f5f2102ece7edce3d21927c628
Juan Lebrijo <juan@lebrijo.com>
2017-04-19 16:49:29 +0200

    Fixing Facebook

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  OmniAuth.config.full_host = "https://#{ENV['domain_name']}"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
