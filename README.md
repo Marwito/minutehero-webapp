9f16a5f8465d5606b6b73b5ac8ec459b2892d7a5
Marwan <marwanelmezni@gmail.com>
2017-06-26 10:51:21 +0200

    feat: email notifications on calls
    https://trello.com/c/apS41XVu

--- README.md
+++ README.md
@@ -3,24 +3,78 @@ MinuteHero WWW
 
 This application requires:
 
-- Ruby 2.3.1
-- Rails 5.0.0.1
+- Ruby 2.3.3
+- Rails 5.0.1
 
-Getting Started
----------------
+Setting up the development environment
+--------------------------------------
 
-Using [RVM](http://blog.lebrijo.com/install-rvm-in-your-ubuntu-desktop/) to define ruby version and gemset:
- 
-1. bundle install
-1. Provide PostgreSQL config in `config/database.yml` file. You have `config/database.example.yml` as example
-1. rake db:drop
-1. rake db:create
-1. rake db:migrate
-1. rake db:seed
-1. guard
+### Install RVM
 
-Admin account by default: 
-* admin_email: admin@minutehero.com
+* \curl -sSL https://raw.githubusercontent.com/wayneeseguin/rvm/stable/binscripts/rvm-installer | sudo bash -s stable
+* source ~/.rvm/scripts/rvm # to be added to .bashrc
+* type rvm | head -n 1
+* rvm install 2.3.3
+
+* sudo usermod -a -G rvm $USER
+
+### Install NVM
+
+* sudo apt-get install build-essential checkinstall
+* sudo apt-get install libssl-dev
+* curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
+nvm install 6.0
+
+### Install bower
+
+* sudo npm install -g bower
+
+### Install bundler
+
+* gem install bundler
+* bundle install
+
+### Install Postgres on Ubuntu 41.04
+
+* for other versions : https://askubuntu.com/questions/831292/how-to-install-postgresql-9-6-on-any-ubuntu-version
+
+* sudo add-apt-repository "deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main"
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+* sudo apt-get update
+* sudo apt-get install postgresql-9.6
+* sudo apt install libpq-dev
+* sudo -u postgres psql 
+* \password
+* postgres
+
+### rake db:drop 
+
+only if you already have an existent database
+
+### rake db:create
+### rake db:migrate
+### rake db:seed
+### phantomjs
+
+Needed to run the automated regression tests using rspec:
+* sudo apt install phantomjs
+### Email notification configuration
+
+We use AWS SES to receive email notifications from the app.
+
+1. Install AWS SDK for Ruby
+gem install aws-sdk
+2. Get your AWS credentials and save them respectively in the variables : AWS_ACCESS_KEY_ID and 
+AWS_SECRET_ACCESS_KEY in the file 'application.yml'
+3. You can edit the email addresses you want to receive notifications for using the variable 'send_call_notifications_to_email' in the file 'application.yml' too. 
+4. Verify these email addresses with Amazon SES.
+
+### guard
+
+To launch the web server by default.
+
+### Admin account by default: 
+* admin_email: admin@minutehero.net
 * admin_password: minher0!
 
 Documentation and Support
--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -29,6 +29,7 @@ class CallsController < ApplicationController
 
     if @call.save
       redirect_to calls_path, notice: 'Call was successfully created.'
+      send_email_notification_to_admin :create
     else
       render :new
     end
@@ -38,6 +39,7 @@ class CallsController < ApplicationController
   def update
     if @call.update(call_params)
       redirect_to calls_path, notice: 'Call was successfully updated.'
+      send_email_notification_to_admin :update
     else
       render :edit
     end
@@ -47,6 +49,7 @@ class CallsController < ApplicationController
   def destroy
     @call.destroy
     redirect_to calls_url, notice: 'Call was successfully deleted.'
+    send_email_notification_to_admin :destroy
   end
 
   private
@@ -71,6 +74,125 @@ class CallsController < ApplicationController
       @calls.order("#{params[:column]} #{params[:sort]}")
     else
       @calls.order(date_time: :desc)
+    end
+  end
+
+  def send_email_notification_to_admin(type_of_action)
+    require 'aws-sdk'
+    sender = ENV['send_call_notifications_to_email']
+    recipient = ENV['send_call_notifications_to_email']
+
+    # Replace us-west-2 with the AWS Region you're using for Amazon SES.
+    awsregion = 'eu-west-1'
+    link_to_call = "<p>Click <a href='http://#{ENV['domain_name']}/calls/#{@call.id}/edit'>here</a>
+       to know the exact time zone of #{@call.time_zone}.</p>"
+    # The subject line for the email.
+    if type_of_action == :create
+      subject = "Call Scheduled: #{@call.schedule_date} #{@call.schedule_time.strftime '%H:%M'}
+      #{@call.time_zone}, #{@call.user.first_name} #{@call.user.last_name}, #{@call.title}"
+      link_to_call_in_html_body = link_to_call
+    elsif type_of_action == :update
+      subject = "Call Edited: #{@call.schedule_date} #{@call.schedule_time.strftime '%H:%M'}
+      #{@call.time_zone}, #{@call.user.first_name} #{@call.user.last_name}, #{@call.title}"
+      link_to_call_in_html_body = link_to_call
+    elsif type_of_action == :destroy
+      subject = "Call Deleted: #{@call.schedule_date} #{@call.schedule_time.strftime '%H:%M'}
+      #{@call.time_zone}, #{@call.user.first_name} #{@call.user.last_name}, #{@call.title}"
+      link_to_call_in_html_body = ''
+    else
+      subject = "Error in processing action of the scheduled call: #{@call.schedule_date}
+      #{@call.schedule_time.strftime '%H:%M'} #{@call.time_zone} #{@call.user.first_name}
+      #{@call.user.last_name}, #{@call.title}"
+      link_to_call_in_html_body = link_to_call
+    end
+    # The HTML body of the email.
+    htmlbody =
+      "<table>
+        <tr>
+            <th>Id</th>
+            <td> #{@call.id} </td>
+        </tr>
+        <tr>
+            <th>User</th>
+            <td> #{@call.user.first_name} #{@call.user.last_name}</td>
+        </tr>
+        <tr>
+            <th>Email Address</th>
+            <td> #{@call.user.email}</td>
+        </tr>
+        <tr>
+            <th>Title</th>
+            <td> #{@call.title} </td>
+        </tr>
+        <tr>
+            <th>Dial In</th>
+            <td> #{@call.dial_in} </td>
+        </tr>
+        <tr>
+            <th>Participant Code</th>
+            <td> #{@call.participant_code} </td>
+        </tr>
+        <tr>
+            <th>Schedule Date</th>
+            <td> #{@call.schedule_date} </td>
+        </tr>
+        <tr>
+            <th>Schedule Time</th>
+            <td> #{@call.schedule_time.strftime '%H:%M'} </td>
+        </tr>
+        <tr>
+            <th>Time Zone</th>
+            <td> #{@call.time_zone} </td>
+        </tr>
+
+      </table>
+
+      #{link_to_call_in_html_body}"
+
+    # The email body for recipients with non-HTML email clients.
+    textbody = 'New call has been scheduled !'
+
+    # Specify the text encoding scheme.
+    encoding = 'UTF-8'
+
+    # Create a new SES resource and specify a region
+    ses = Aws::SES::Client.new(region: awsregion)
+
+    # Try to send the email.
+    begin
+
+      # Provide the contents of the email.
+      resp = ses.send_email({
+        destination:  {
+          to_addresses: [
+            recipient
+          ]
+        },
+        message: {
+          body: {
+            html: {
+              charset: encoding,
+              data: htmlbody
+            },
+          text: {
+            charset: encoding,
+            data: textbody
+          }
+          },
+          subject: {
+            charset: encoding,
+            data: subject
+          },
+        }
+      source: sender
+                          })
+      puts 'Email sent!'
+
+    # If something goes wrong, display an error message.
+    rescue => e
+      print 'Email not sent. Error message: '
+      puts e
+
     end
   end
 end
