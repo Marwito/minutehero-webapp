02fcda97e39bebb728889b3427635c4a10b3fb54
Marc Püls <mpuels@gmail.com>
2017-06-27 19:26:01 +0200

    refactor: Encapsulate mail sender in class

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -2,7 +2,7 @@ class CallsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_call, only: [:show, :edit, :update, :destroy]
   before_action :authorize_calls, only: [:index, :new, :create]
-  attr_reader :call
+
   # GET /calls
   def index
     @calls = CallPolicy::Scope.new(current_user, Call).resolve
@@ -29,7 +29,8 @@ class CallsController < ApplicationController
 
     if @call.save
       redirect_to calls_path, notice: 'Call was successfully created.'
-      send_email_notification_to_admin(:create, @call)
+      mn = MailNotifier.new @call, :create
+      mn.send
     else
       render :new
     end
@@ -39,7 +40,8 @@ class CallsController < ApplicationController
   def update
     if @call.update(call_params)
       redirect_to calls_path, notice: 'Call was successfully updated.'
-      send_email_notification_to_admin(:update, @call)
+      mn = MailNotifier.new @call, :update
+      mn.send
     else
       render :edit
     end
@@ -49,7 +51,8 @@ class CallsController < ApplicationController
   def destroy
     @call.destroy
     redirect_to calls_url, notice: 'Call was successfully deleted.'
-    send_email_notification_to_admin(:destroy, @call)
+    mn = MailNotifier.new @call, :destroy
+    mn.send
   end
 
   private
@@ -78,123 +81,72 @@ class CallsController < ApplicationController
   end
 end
 
-def send_email_notification_to_admin(type_of_action, call)
-  require 'aws-sdk'
-  sender = ENV['send_call_notifications_to_email']
-  recipient = ENV['send_call_notifications_to_email']
-  awsregion = ENV['aws_region']
-  subject, link_to_call_in_html_body = subject_and_link(type_of_action, call)
-  # The HTML body of the email.
-  htmlbody =
-    "<table>
-      <tr>
-          <th>Id</th>
-          <td> #{call.id} </td>
-      </tr>
-      <tr>
-          <th>User</th>
-          <td> #{call.user.first_name} #{call.user.last_name}</td>
-      </tr>
-      <tr>
-          <th>Email Address</th>
-          <td> #{call.user.email}</td>
-      </tr>
-      <tr>
-          <th>Title</th>
-          <td> #{call.title} </td>
-      </tr>
-      <tr>
-          <th>Dial In</th>
-          <td> #{call.dial_in} </td>
-      </tr>
-      <tr>
-          <th>Participant Code</th>
-          <td> #{call.participant_code} </td>
-      </tr>
-      <tr>
-          <th>Schedule Date</th>
-          <td> #{call.schedule_date} </td>
-      </tr>
-      <tr>
-          <th>Schedule Time</th>
-          <td> #{call.schedule_time.strftime '%H:%M'} </td>
-      </tr>
-      <tr>
-          <th>Time Zone</th>
-          <td> #{call.time_zone} </td>
-      </tr>
-
-    </table>
-
-    #{link_to_call_in_html_body}"
-
-  # The email body for recipients with non-HTML email clients.
-  textbody = 'New call has been scheduled !'
-
-  # Specify the text encoding scheme.
-  encoding = 'UTF-8'
-
-  # Create a new SES resource and specify a region
-  ses = Aws::SES::Client.new(region: awsregion)
-
-  # Try to send the email.
-  begin
-
-    # Provide the contents of the email.
-    ses.send_email(
-      destination: {
-        to_addresses: [
-          recipient
-        ]
-      },
-      message: {
-        body: {
-          html: {
-            charset: encoding,
-            data: htmlbody
-          },
-        text: {
-          charset: encoding,
-          data: textbody
-        }
-        },
-        subject: {
-          charset: encoding,
-          data: subject
-        }
-      },
-      source: sender
-    )
-
-  # If something goes wrong, display an error message.
-  rescue => e
-    logger.error "Email not sent. Reason : #{e}"
-
+class MailNotifier
+  def initialize(call, type_of_action)
+    @call = call
+    @type_of_action = type_of_action
+    @aws_region = ENV['aws_region']
+    @sender = ENV['send_call_notifications_to_email']
+    @recipient = ENV['send_call_notifications_to_email']
   end
-end
 
-def subject_and_link(type_of_action, call)
-  link_to_call = "<p>Click <a href='http://#{ENV['domain_name']}/calls/" \
-                  "#{call.id}/edit'>here</a> to know the exact time zone of " \
-                  "#{call.time_zone}.</p>"
-
-  common_subj = "#{call.schedule_date} " \
-                "#{call.schedule_time.strftime '%H:%M'} " \
-                "#{call.time_zone}, #{call.user.first_name} " \
-                "#{call.user.last_name}, #{call.title}"
-  # The subject line for the email.
-  if type_of_action == :create
-    subject = 'Call Scheduled: ' + common_subj
-    link_to_call_in_html_body = link_to_call
-  elsif type_of_action == :update
-    subject = 'Call Edited: ' + common_subj
-    link_to_call_in_html_body = link_to_call
-  elsif type_of_action == :destroy
-    subject = 'Call Deleted: ' + common_subj
-    link_to_call_in_html_body = ''
-  else
-    subject = 'Error in processing action of the scheduled call: ' + common_subj
-    link_to_call_in_html_body = link_to_call
+  def send
+    require 'aws-sdk'
+    ses = Aws::SES::Client.new(region: @aws_region)
+    begin
+      ses.send_email ses_send_email_arg
+    rescue => e
+      Rails.logger.error "Email not sent. Reason : #{e}"
+    end
   end
-  [subject, link_to_call_in_html_body]
+
+  private
+
+  def ses_send_email_arg
+    encoding = 'UTF-8'
+    { destination: { to_addresses: [@recipient] },
+      message: { subject: { charset: encoding, data: subject },
+                 body: { html: { charset: encoding, data: htmlbody } } },
+      source: @sender }
+  end
+
+  def subject
+    call_action = case @type_of_action
+                  when :create then 'Scheduled'
+                  when :update then 'Edited'
+                  when :destroy then 'Deleted'
+                  end
+    "Call #{call_action}: #{@call.schedule_date} " \
+      "#{@call.schedule_time.strftime '%H:%M'} " \
+      "#{@call.time_zone}, #{@call.user.first_name} " \
+      "#{@call.user.last_name}, #{@call.title}"
+  end
+
+  def htmlbody
+    body = '<table>'
+    table_rows.each do |key, value|
+      body += "<tr><th>#{key}</th><td>#{value}</td></tr>"
+    end
+    body += '</table>'
+    [:create, :update].include?(@type_of_action) && body += link_to_call
+    body
+  end
+
+  def table_rows
+    [['Id', @call.id],
+     ['User', "#{@call.user.first_name} #{@call.user.last_name}"],
+     ['Email Address', @call.user.email],
+     ['Title', @call.title],
+     ['Dial In', @call.dial_in],
+     ['Participant Code', @call.participant_code],
+     ['Schedule Date', @call.schedule_date.to_s],
+     ['Schedule Time', @call.schedule_time.strftime('%H:%M')],
+     ['Time Zone', @call.time_zone]]
+  end
+
+  def link_to_call
+    "<p>Click <a href='http://#{ENV['domain_name']}/calls/" \
+      "#{@call.id}/edit'>here</a> to know the exact time zone of " \
+      "#{@call.time_zone}.</p>"
+  end
 end
