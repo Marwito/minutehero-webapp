07b9b2274f0fc9a3c9aefb784628457b8cef68dd
Marwan <marwanelmezni@gmail.com>
2017-06-27 15:19:45 +0200

    feat: email notifications on calls : review 2

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -2,7 +2,7 @@ class CallsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_call, only: [:show, :edit, :update, :destroy]
   before_action :authorize_calls, only: [:index, :new, :create]
-
+  attr_reader :call
   # GET /calls
   def index
     @calls = CallPolicy::Scope.new(current_user, Call).resolve
@@ -29,7 +29,7 @@ class CallsController < ApplicationController
 
     if @call.save
       redirect_to calls_path, notice: 'Call was successfully created.'
-      send_email_notification_to_admin :create
+      send_email_notification_to_admin(:create, @call)
     else
       render :new
     end
@@ -39,7 +39,7 @@ class CallsController < ApplicationController
   def update
     if @call.update(call_params)
       redirect_to calls_path, notice: 'Call was successfully updated.'
-      send_email_notification_to_admin :update
+      send_email_notification_to_admin(:update, @call)
     else
       render :edit
     end
@@ -49,7 +49,7 @@ class CallsController < ApplicationController
   def destroy
     @call.destroy
     redirect_to calls_url, notice: 'Call was successfully deleted.'
-    send_email_notification_to_admin :destroy
+    send_email_notification_to_admin(:destroy, @call)
   end
 
   private
@@ -76,117 +76,125 @@ class CallsController < ApplicationController
       @calls.order(date_time: :desc)
     end
   end
+end
 
-  def send_email_notification_to_admin(type_of_action)
-    require 'aws-sdk'
-    sender = ENV['send_call_notifications_to_email']
-    recipient = ENV['send_call_notifications_to_email']
-    awsregion = ENV['aws_region']
+def send_email_notification_to_admin(type_of_action, call)
+  require 'aws-sdk'
+  sender = ENV['send_call_notifications_to_email']
+  recipient = ENV['send_call_notifications_to_email']
+  awsregion = ENV['aws_region']
+  subject, link_to_call_in_html_body = subject_and_link(type_of_action, call)
+  # The HTML body of the email.
+  htmlbody =
+    "<table>
+      <tr>
+          <th>Id</th>
+          <td> #{call.id} </td>
+      </tr>
+      <tr>
+          <th>User</th>
+          <td> #{call.user.first_name} #{call.user.last_name}</td>
+      </tr>
+      <tr>
+          <th>Email Address</th>
+          <td> #{call.user.email}</td>
+      </tr>
+      <tr>
+          <th>Title</th>
+          <td> #{call.title} </td>
+      </tr>
+      <tr>
+          <th>Dial In</th>
+          <td> #{call.dial_in} </td>
+      </tr>
+      <tr>
+          <th>Participant Code</th>
+          <td> #{call.participant_code} </td>
+      </tr>
+      <tr>
+          <th>Schedule Date</th>
+          <td> #{call.schedule_date} </td>
+      </tr>
+      <tr>
+          <th>Schedule Time</th>
+          <td> #{call.schedule_time.strftime '%H:%M'} </td>
+      </tr>
+      <tr>
+          <th>Time Zone</th>
+          <td> #{call.time_zone} </td>
+      </tr>
 
-    link_to_call = "<p>Click <a href='http://#{ENV['domain_name']}/calls/#{@call.id}/edit'>here</a>
-       to know the exact time zone of #{@call.time_zone}.</p>"
-    common_subject = "#{@call.schedule_date} #{@call.schedule_time.strftime '%H:%M'}
-      #{@call.time_zone}, #{@call.user.first_name} #{@call.user.last_name}, #{@call.title}"
-    # The subject line for the email.
-    if type_of_action == :create
-      subject = "Call Scheduled: " + common_subject
-      link_to_call_in_html_body = link_to_call
-    elsif type_of_action == :update
-      subject = "Call Edited: " + common_subject
-      link_to_call_in_html_body = link_to_call
-    elsif type_of_action == :destroy
-      subject = "Call Deleted:" + common_subject
-      link_to_call_in_html_body = ''
-    else
-      subject = "Error in processing action of the scheduled call: "+ common_subject
-      link_to_call_in_html_body = link_to_call
-    end
-    # The HTML body of the email.
-    htmlbody =
-      "<table>
-        <tr>
-            <th>Id</th>
-            <td> #{@call.id} </td>
-        </tr>
-        <tr>
-            <th>User</th>
-            <td> #{@call.user.first_name} #{@call.user.last_name}</td>
-        </tr>
-        <tr>
-            <th>Email Address</th>
-            <td> #{@call.user.email}</td>
-        </tr>
-        <tr>
-            <th>Title</th>
-            <td> #{@call.title} </td>
-        </tr>
-        <tr>
-            <th>Dial In</th>
-            <td> #{@call.dial_in} </td>
-        </tr>
-        <tr>
-            <th>Participant Code</th>
-            <td> #{@call.participant_code} </td>
-        </tr>
-        <tr>
-            <th>Schedule Date</th>
-            <td> #{@call.schedule_date} </td>
-        </tr>
-        <tr>
-            <th>Schedule Time</th>
-            <td> #{@call.schedule_time.strftime '%H:%M'} </td>
-        </tr>
-        <tr>
-            <th>Time Zone</th>
-            <td> #{@call.time_zone} </td>
-        </tr>
+    </table>
 
-      </table>
+    #{link_to_call_in_html_body}"
 
-      #{link_to_call_in_html_body}"
+  # The email body for recipients with non-HTML email clients.
+  textbody = 'New call has been scheduled !'
 
-    # The email body for recipients with non-HTML email clients.
-    textbody = 'New call has been scheduled !'
+  # Specify the text encoding scheme.
+  encoding = 'UTF-8'
 
-    # Specify the text encoding scheme.
-    encoding = 'UTF-8'
+  # Create a new SES resource and specify a region
+  ses = Aws::SES::Client.new(region: awsregion)
 
-    # Create a new SES resource and specify a region
-    ses = Aws::SES::Client.new(region: awsregion)
+  # Try to send the email.
+  begin
 
-    # Try to send the email.
-    begin
-
-      # Provide the contents of the email.
-      resp = ses.send_email({
-        destination:  {
-          to_addresses: [
-            recipient
-          ]
-        },
-        message: {
-          body: {
-            html: {
-              charset: encoding,
-              data: htmlbody
-            },
-          text: {
+    # Provide the contents of the email.
+    ses.send_email(
+      destination: {
+        to_addresses: [
+          recipient
+        ]
+      },
+      message: {
+        body: {
+          html: {
             charset: encoding,
-            data: textbody
-          }
+            data: htmlbody
           },
-          subject: {
-            charset: encoding,
-            data: subject
-          },
+        text: {
+          charset: encoding,
+          data: textbody
+        }
         },
+        subject: {
+          charset: encoding,
+          data: subject
+        }
+      },
       source: sender
-                          })
+    )
 
-    # If something goes wrong, display an error message.
-    rescue => e
-      puts "Email not sent. Error message : #{e}"
+  # If something goes wrong, display an error message.
+  rescue => e
+    logger.error "Email not sent. Reason : #{e}"
 
-    end
   end
+end
+
+def subject_and_link(type_of_action, call)
+  link_to_call = "<p>Click <a href='http://#{ENV['domain_name']}/calls/" \
+                  "#{call.id}/edit'>here</a> to know the exact time zone of " \
+                  "#{call.time_zone}.</p>"
+
+  common_subj = "#{call.schedule_date} " \
+                "#{call.schedule_time.strftime '%H:%M'} " \
+                "#{call.time_zone}, #{call.user.first_name} " \
+                "#{call.user.last_name}, #{call.title}"
+  # The subject line for the email.
+  if type_of_action == :create
+    subject = 'Call Scheduled: ' + common_subj
+    link_to_call_in_html_body = link_to_call
+  elsif type_of_action == :update
+    subject = 'Call Edited: ' + common_subj
+    link_to_call_in_html_body = link_to_call
+  elsif type_of_action == :destroy
+    subject = 'Call Deleted: ' + common_subj
+    link_to_call_in_html_body = ''
+  else
+    subject = 'Error in processing action of the scheduled call: ' + common_subj
+    link_to_call_in_html_body = link_to_call
+  end
+  [subject, link_to_call_in_html_body]
 end
