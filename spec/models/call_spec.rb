071145ae764e4e4d3c41fff4348458b1e9e9a999
Marwan <marwanelmezni@gmail.com>
2017-07-03 11:26:11 +0200

    feat: users can only view their calls
    An admin can schedule, edit and delete calls

--- spec/models/call_spec.rb
+++ spec/models/call_spec.rb
@@ -9,11 +9,26 @@ describe Call, type: :model do
   it { should validate_presence_of :schedule_time }
   it { should validate_presence_of :user_id }
 
-  it 'should validate :date_time not in the past' do
-    call.date_time = 2.days.ago
-    expect(call).not_to be_valid
-    expect(call.errors[:date_time]).to include
-    I18n.t('call.errors.date_time.past')
+  describe 'should validate :date_time not in the past' do
+    now = Time.new(2017, 6, 23, 11, 50, 0, '+00:00')
+    time_zone = 'Brasilia' # UTC-03:00
+    Timecop.freeze(now) do
+      it "When now=#{now} then 2017-06-23 10:50 UTC-03:00 not in the past" do
+        call.schedule_date = Date.new(2017, 6, 23)
+        call.schedule_time = Time.new(1, 1, 1, 10, 50, 0)
+        call.time_zone = time_zone
+        expect(call).to be_valid
+      end
+
+      it "When now=#{now} then 2017-06-23 07:50 UTC-03:00 in the past" do
+        call.schedule_date = Date.new(2017, 6, 23)
+        call.schedule_time = Time.new(1, 1, 1, 7, 50, 0)
+        call.time_zone = time_zone
+        expect(call).not_to be_valid
+        expect(call.errors[:date_time]).to include
+        I18n.t('call.errors.date_time.past')
+      end
+    end
   end
 
   describe 'should validate if valid phone' do
