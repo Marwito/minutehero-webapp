88d6a3f4535eeceafbb2067e95d16e11d3a4106a
Marwan <marwanelmezni@gmail.com>
2017-07-04 17:44:30 +0200

    chore: resolve merge conflict in config/application.yml

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -3,7 +3,6 @@ class User
     prepend_before_action :check_captcha, only: [:create]
 
     def after_inactive_sign_up_path_for(resource)
-      set_flash_message! :notice, ''
       page_path 'confirm', email: resource.email
     end
 
