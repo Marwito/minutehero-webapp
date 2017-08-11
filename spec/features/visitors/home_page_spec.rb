7a428a250b31adfbce14e5b5a22813c436398981
Marwito <marwanelmezni@gmail.com>
2017-08-11 16:52:14 +0200

    fix: correct errors found by rspec (#32)
    Closes #31

--- spec/features/visitors/home_page_spec.rb
+++ spec/features/visitors/home_page_spec.rb
@@ -2,13 +2,13 @@
 #   As a visitor
 #   I want to visit a home page
 #   So I can learn more about the website
-feature 'Home page' do
+feature 'Redirection to the sign-in page' do
   # Scenario: Visit the home page
   #   Given I am a visitor
-  #   When I visit the home page
-  #   Then I see "Welcome"
-  scenario 'visit the home page' do
+  #   When I access the website, i will be asked to sign in or sign up
+  #   Then I will see "sign in"
+  scenario 'visit the sign_in page' do
     visit root_path
-    expect(page).to have_content 'Tired of taking notes?'
+    expect(page).to have_content 'sign in'
   end
 end
--- spec/support/helpers/session_helpers.rb
+++ spec/support/helpers/session_helpers.rb
@@ -2,10 +2,10 @@ module Features
   module SessionHelpers
     def sign_up_with(email, password)
       visit new_user_registration_path
-      fill_in '* First Name', with: 'Juan'
-      fill_in '* Last Name', with: 'Perro'
-      fill_in '* Email', with: email
-      fill_in '* Password', with: password
+      fill_in 'First Name *', with: 'Juan'
+      fill_in 'Last Name *', with: 'Perro'
+      fill_in 'Email *', with: email
+      fill_in 'Password *', with: password
       find(:css, '.custom-control-indicator').trigger('click')
       click_button 'Sign up'
     end
